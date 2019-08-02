80dade18305e54e3b3978236d93b4958bb945c0c
Anastasiya Zuyeva <anastasiya.zuyeva-extern@deutschebahn.com>
2019-08-02 14:17:58 +0200

    - initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length == 0) {
+            throw new Exception("Gebe, bitte, einen Suchbergriff ein!");
+        }
+
+        final String searchRequest = args[0];
+        int number = args.length > 1 ? Integer.parseInt(args[1]) : 3;
+        String searchEngine = args.length > 2 ? args[2] : "google.de";
+
+        WebDriver driver = null;
+        try {
+            System.setProperty("webdriver.chrome.driver", Main.class.getResource("chromedriver.exe").getPath());
+            driver = new ChromeDriver();
+            driver.get("http://" + searchEngine);
+            driver.manage().window().maximize();
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
+            element.sendKeys(searchRequest);
+            element.submit();
+            WebElement results = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("resultStats")));
+            final String resultOutput = results.getText();
+            System.out.println("Results: " + resultOutput);
+            List<WebElement> links = driver.findElements(By.xpath("//div[@id='rso']//div[@class='r']/a[1]"));
+
+            for (int i = 0; i < number; i++) {
+                System.out.println(links.get(i).getAttribute("href"));
+            }
+
+            driver.quit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+}
